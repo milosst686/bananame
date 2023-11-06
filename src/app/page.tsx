a66cd6df2fe050c3d5a6c320b03aa2f4a88da352
milosst686 <milosst686@hotmail.com>
2023-11-06 20:22:44 +0100

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,113 @@
-import Image from 'next/image'
+"use client"
+import React, { useCallback, useRef, useState } from 'react'
+import Image from 'next/image';
+import BananaMe from '../../public/BananaMe.png';
+import Link from 'next/link';
+import Bananas from '../../public/BCATBananas.png';
+import UpOnly from '../../public/BCATUpOnly.png';
+import Windows from '../../public/BCATWindows.png';
+import { TwitterIcon, TwitterShareButton } from 'react-share';
 
 export default function Home() {
+  const imageList = [
+    Bananas,
+    UpOnly,
+    Windows,
+]
+const [file , setFile] = useState<File>();
+const [imageUrl , setImageUrl] = useState("");
+
+const [imageNum, setImageNum] = useState(0);
+const handleIncrease = () => {
+  if(imageNum == 2)
+    setImageNum(0);
+  else
+  setImageNum(imageNum +1)
+}
+const handleDecrease = () => {
+  if(imageNum == 0)
+    setImageNum(2);
+  else
+  setImageNum(imageNum -1)
+}
+const [currentPageUrl, setCurrentUrl] = useState("") ;
+
+const combinedImageRef = useRef<HTMLCanvasElement>(null);
+
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main >
+       <div className="flex min-h-screen items-center flex-col w-full bg-black">
+       <div className='flex justify-center pt-5'>
+       <Image src={BananaMe} id="BananaMeText" alt="BananaMe" width={329} height={96}/>
         </div>
-      </div>
+        <div className='flex justify-center items-center'>
+        <button onClick={handleIncrease} className='w-[100px] h-[100px] text-white text-7xl font-bold' > {"<"}</button>
+        <Image src={imageList[imageNum]} width={500} height={500} alt='/' className='z-10'/>
+        <Image src={imageUrl} width={200} height={200} layout='-1' alt='/' className='absolute rounded-full'/>
+        <button onClick={handleDecrease} className='w-[100px] h-[100px] text-white text-7xl font-bold' > {">"}</button>
+        </div>
+        <h2 className='p-5 text-white'>Upload your face and adjust it to fit the banana costume!</h2>
+       <div className='flex justify-center items-center'>
+      <form action="" onClick={() => document.querySelector(".input")}>
+      <input id='imageInput' onChange={({target}) =>{
+            if(target.files)
+            {
+                const file = target.files[0];
+                setImageUrl (URL.createObjectURL(file));
+                setFile(file);
+                const reader = new FileReader();
+               reader.addEventListener("load", () => {
+                console.log(reader.result);
+               })
+               reader.readAsDataURL(file);
+            }
+            else setImageUrl("");
+          }            
+         } 
+    type="file" className='text-lg font-bold w-[120px] mx-10'/>
+      </form>
+        <button onClick={async () => {
+  if (imageList[imageNum] && imageUrl) {
+    const formData = new FormData();
+    formData.append('image1', imageList[imageNum].src );
+    formData.append('image2', imageUrl);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const response = await fetch('/api/combineImages', {
+      method: 'POST',
+      body: formData,
+    });
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    if (response.ok) {
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'combined_image.png';
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    }
+  }}}
+className='w-[120px] text-lg font-bold h-[34px] bg-slate-50 mx-10'  >Save</button>
+        <canvas
+        ref={combinedImageRef}
+        style={{ display: 'none' }}
+        width="128" // Width of the combined image
+        height="128" // Height of the combined image
+      ></canvas>
+       </div>
+        <TwitterShareButton url={imageUrl} >
+          <button className='w-[120px] text-lg font-bold h-[34px] bg-slate-50 my-10'  >Share on X</button>
+        </TwitterShareButton>
+        <Link href="https://swap.defillama.com/?chain=ethereum&from=0x0000000000000000000000000000000000000000&to=0x0590cc9232ebf68d81f6707a119898219342ecb9" target="_blank" >
+        <button className='w-[120px] text-lg font-bold h-[34px] bg-slate-50 '>Buy $BCAT</button>
+        </Link>
+       
+       </div>
+      
     </main>
   )
 }
